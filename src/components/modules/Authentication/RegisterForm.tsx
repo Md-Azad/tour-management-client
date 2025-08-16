358486b8a561dbabbd481acf0733a5d70d9d48b2
Md Azad Hossain <sokalazad@gmail.com>
2025-08-16 22:18:29 +0200

    register page done

--- src/components/modules/Authentication/RegisterForm.tsx
+++ src/components/modules/Authentication/RegisterForm.tsx
@@ -1,11 +1,56 @@
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Password from "@/components/ui/password";
+
+const registationSchema = z
+  .object({
+    name: z.string().min(3, { error: "name is too short" }).max(50),
+    email: z.email({ error: "Must be a valid Email" }),
+    password: z
+      .string()
+      .min(8, { error: "password must be atleast 8 character" })
+      .max(20, { error: "password must be under 21 character" }),
+    confirmPassword: z
+      .string()
+      .min(8, { error: "password must be atleast 8 character" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
 
 const RegisterForm = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
+  const form = useForm<z.infer<typeof registationSchema>>({
+    resolver: zodResolver(registationSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data: z.infer<typeof registationSchema>) => {
+    console.log("object", data);
+  };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
@@ -13,6 +58,87 @@ const RegisterForm = ({
         <p className="text-sm text-muted-foreground">
           Enter your details to create an account
         </p>
+      </div>
+
+      <div className="grid gap-6">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John Doe" type="text" {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="example@company.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public Email.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Password {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public Password Field.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Password {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public confirm password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </Form>
       </div>
       <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
         <span className="relative z-10 bg-background px-2 text-muted-foreground">
