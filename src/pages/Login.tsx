27763440f172ceaeca179d2d1cd2a0def212e782
Md Azad Hossain <sokalazad@gmail.com>
2025-08-21 22:08:48 +0200

    refector login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import TravelLogin from "@/assets/images/travel-login.jpg";
 import { Link } from "react-router";
 import Logo from "@/assets/icons/Logo";
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/ui/login-form";
 
 export default function Login() {
   return (
